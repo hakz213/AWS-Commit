4abfd397aaca24d521a41490fbd426c20936aa4d
Lekë Vranica <exp.titan@gmail.com>
2025-09-28 21:40:11 +0200

    Initial commit

---
+++ lib/my_cdk_project-stack.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+  
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class MyCdkProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
+      runtime: lambda.Runtime.PYTHON_3_13,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "main.handler",
+    });
+
+    const functionUrl = lambdaFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "Url", {
+      value: functionUrl.url,
+    });
+  }
+}
